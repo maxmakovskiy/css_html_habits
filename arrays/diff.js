bd6d9df32b637993a005beeed8e78afa6ecaa24f
maxmakovskiy <makovskiymax@gmail.com>
2022-01-22 11:34:07 +0200

    difference

---
+++ arrays/diff.js
@@ -0,0 +1,47 @@
+// Overview:
+// Creates an array of array values not included in the other
+// given array.
+// The order and references of result values are determined
+// by the first array.
+// Examples:
+// difference([2, 1], [2, 3]); => [1]
+
+(function(){
+    "use strict";
+
+    function difference(lhs, rhs) {
+        let result = [];
+
+        lhs.forEach(element => {
+            if (!rhs.includes(element)) {
+                result.push(element);
+            }
+
+        });
+
+        return result;
+    }
+
+    function compareArrays(arr1, arr2)
+    {
+        if (arr1.length !== arr2.length) return false;
+        
+        for (let i = 0; i < arr1.length; ++i) {
+            if (arr1[i] !== arr2[i]) return false;
+        }
+
+        return true;
+    }
+
+    function testDiff() {
+        const sample1 = [2, 1];
+        const sample2 = [2, 3];
+        const result = difference(sample1, sample2);
+        const expected = [1];
+
+        const res = compareArrays(result, expected);
+        console.log(testDiff.name + (res ? " passed" : " failed"));
+    }
+
+    testDiff();
+})()
